dce384a9c2ac92b6486a71b956f5ad8bc73135a4
Aman Babu Gautam <amanbabugautam@gmail.com>
2016-05-17 10:01:56 +0530

    Delete challenge.py

--- challenge.py
+++
@@ -1,7 +0,0 @@
-# from random import randint
-
-# def getname(name):
-# 	return (True, name, randint(6,15))
-# name1 = raw_input()
-# succeeded ,name2, randno = getname(name1)
-# print name2, randno
