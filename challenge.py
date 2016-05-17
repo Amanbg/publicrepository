fa64a39e291ece1d9dcead7c72713c2c43ef4717
Amanbg <amanbabugautam@gmail.com>
2016-05-17 09:49:29 +0530

    code done

---
+++ challenge.py
@@ -0,0 +1,7 @@
+# from random import randint
+
+# def getname(name):
+# 	return (True, name, randint(6,15))
+# name1 = raw_input()
+# succeeded ,name2, randno = getname(name1)
+# print name2, randno
